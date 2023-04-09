7c513ed38c965ed053f722381e1a80e7a5b91859
Hossein Naderi <hossein-naderi@hotmail.com>
2023-04-09 18:19:03 +0330

    YAML tree generation

--- core/src/main/scala/dev/hnaderi/libyaml/Main.scala
+++ core/src/main/scala/dev/hnaderi/libyaml/Main.scala
@@ -27,7 +27,7 @@ import all.*
 object Main {
   def main(args: Array[String]): Unit = Zone { implicit zone =>
     val input =
-      scala.io.Source.fromFile("yaml.org.yaml").getLines().mkString("\n")
+      scala.io.Source.fromFile("test.yaml").getLines().mkString("\n")
     val input_c = toCString(input).asInstanceOf[Ptr[CUnsignedChar]]
 
     val parser = yaml_parser_t()
@@ -41,28 +41,60 @@ object Main {
     if (yaml_parser_load(parser, document) == 0)
       println("Failed to load document!")
 
-    val root = yaml_document_get_root_node(document)
-    visitNode(document, root, Printer)
+    val ytg = YamlTreeGenerator()
+    val doc = LibYamlDocument(document)
+    doc.visit(ytg)
 
-    yaml_document_delete(document)
+    println("Tree:")
+    println(ytg.build)
+
+    doc.clean
     yaml_parser_delete(parser)
   }
+}
 
-  def visitNode(
+trait YAMLParser {
+  def parse(input: String): YAMLDocument
+
+  def clean: Unit
+}
+
+trait YAMLDocument {
+  def visit(visitor: YamlNodeVisitor): Unit
+
+  def clean: Unit
+}
+
+trait YAMLDocumentMutable extends YAMLDocument {
+  def addMapping(): Unit
+}
+
+final class LibYamlDocument(private val document: Ptr[yaml_document_t])
+    extends YAMLDocument {
+
+  override def clean: Unit =
+    yaml_document_delete(document)
+
+  override def visit(visitor: YamlNodeVisitor): Unit = Zone { implicit zone =>
+    val root = yaml_document_get_root_node(document)
+    visitNode(document, root, visitor)
+  }
+
+  private def visitNode(
       document: Ptr[yaml_document_t],
       root: Ptr[yaml_node_t],
-      visitor: YamlVisitor
+      visitor: YamlNodeVisitor
   )(using Zone): Unit = {
     val node = !root
     val nodeType = yaml_node_type_t(node.`type`)
 
     nodeType match {
       case yaml_node_type_e.YAML_MAPPING_NODE =>
-        visitMapping(document, node.data.mapping, visitor)
+        visitMapping(document, node.data.mapping, visitor.mapping())
       case yaml_node_type_e.YAML_SEQUENCE_NODE =>
-        visitSeq(document, node.data.sequence, visitor)
+        visitSeq(document, node.data.sequence, visitor.seq())
       case yaml_node_type_e.YAML_SCALAR_NODE =>
-        visitScalar(node.data.scalar, visitor)
+        visitScalar(node.data.scalar, visitor.scalar())
       case yaml_node_type_e.YAML_NO_NODE =>
         println("No node!!!")
     }
@@ -78,53 +110,51 @@ object Main {
     new String(bytes)
   }
 
-  def visitScalar(
+  private def visitScalar(
       node: yaml_node_t.Union0.Struct0,
-      visitor: YamlVisitor
+      visitor: YamlScalarVisitor
   ): Unit =
-    visitor.onScalar(scalarValue(node))
+    visitor.write(scalarValue(node))
 
-  def visitSeq(
+  private def visitSeq(
       document: Ptr[yaml_document_t],
       node: yaml_node_t.Union0.Struct1,
-      visitor: YamlVisitor
+      visitor: YamlSequenceVisitor
   )(using Zone): Unit = {
     val items = node.items
-    visitor.startSeq()
     var cur = items.start
     while (cur.toLong < items.top.toLong) {
       val idx = (!cur).value
       val node = yaml_document_get_node(document, idx)
-      visitNode(document, node, visitor)
+      visitNode(document, node, visitor.next())
       cur = cur + 1
     }
 
-    visitor.endSeq()
+    visitor.`end`()
   }
 
-  def visitMapping(
+  private def visitMapping(
       document: Ptr[yaml_document_t],
       node: yaml_node_t.Union0.Struct2,
-      visitor: YamlVisitor
+      visitor: YamlMappingVisitor
   )(using
       Zone
   ): Unit = {
     val items = node.pairs
-    visitor.startMapping()
     var cur = items.start
     while (cur.toLong < items.top.toLong) {
       val keyIdx = (!cur).key
       val key = yaml_document_get_node(document, keyIdx)
       assert((!key).`type` == yaml_node_type_e.YAML_SCALAR_NODE)
 
-      visitor.goto(scalarValue((!key).data.scalar))
+      val current = visitor.at(scalarValue((!key).data.scalar))
 
       val idx = (!cur).value
       val node = yaml_document_get_node(document, idx)
-      visitNode(document, node, visitor)
+      visitNode(document, node, current)
       cur = cur + 1
     }
 
-    visitor.endMapping()
+    visitor.`end`()
   }
 }
---
+++ core/src/main/scala/dev/hnaderi/libyaml/YAML.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2023 Hossein Naderi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.hnaderi.libyaml
+
+import scala.collection.mutable
+
+trait YamlNodeVisitor {
+  def scalar(): YamlScalarVisitor
+  def seq(): YamlSequenceVisitor
+  def mapping(): YamlMappingVisitor
+}
+
+trait YamlScalarVisitor {
+  def write(value: String): Unit
+}
+trait YamlSequenceVisitor {
+  def next(): YamlNodeVisitor
+  def end(): Unit
+}
+trait YamlMappingVisitor {
+  def at(key: String): YamlNodeVisitor
+  def end(): Unit
+}
+
+sealed trait YAML extends Any
+object YAML {
+  case object Null extends YAML
+  final class Scalar private[YAML] (val value: String)
+      extends AnyVal
+      with YAML {
+    override def toString(): String = value
+  }
+  final class Mapping private[YAML] (
+      private val value: mutable.Map[String, YAML]
+  ) extends AnyVal
+      with YAML {
+    private[libyaml] def add(key: String, elem: YAML): Mapping = {
+      value.addOne((key, elem))
+      this
+    }
+
+    def items: Map[String, YAML] = value.toMap
+
+    override def toString(): String = value.toString()
+  }
+  final class Sequence private[YAML] (
+      private val value: mutable.ListBuffer[YAML]
+  ) extends AnyVal
+      with YAML {
+    private[libyaml] def add(elem: YAML): Sequence = {
+      value.append(elem)
+      this
+    }
+    private[libyaml] def addAll(elems: IterableOnce[YAML]): Sequence = {
+      value.appendAll(elems)
+      this
+    }
+
+    def items: List[YAML] = value.toList
+
+    override def toString(): String = value.toString()
+  }
+
+  def apply(value: String): Scalar = new Scalar(value)
+  def sequence(values: YAML*): Sequence = new Sequence(
+    mutable.ListBuffer.from(values)
+  )
+  def sequence(values: IterableOnce[YAML]): Sequence = new Sequence(
+    mutable.ListBuffer.from(values)
+  )
+  def mapping(values: (String, YAML)*): Mapping = new Mapping(
+    mutable.Map.from(values)
+  )
+  def mapping(values: IterableOnce[(String, YAML)]): Mapping = new Mapping(
+    mutable.Map.from(values)
+  )
+}
