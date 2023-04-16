2e01a415b78987c797fa6057c400e24fc7be1d3d
Hossein Naderi <hossein-naderi@hotmail.com>
2023-04-16 10:02:11 +0330

    Json4s visitor

--- modules/json4s/src/main/scala/Json4s.scala
+++ modules/json4s/src/main/scala/Json4s.scala
@@ -23,37 +23,43 @@ import org.json4s.JDouble
 import org.json4s.JField
 import org.json4s.JInt
 import org.json4s.JLong
+import org.json4s.JNothing
 import org.json4s.JNull
 import org.json4s.JObject
+import org.json4s.JSet
 import org.json4s.JString
 import org.json4s.JValue
 
 package object json4s {
   implicit object Json4sWriter extends Writer[JValue] {
-
     override def ynull: JValue = JNull
-
     override def yfalse: JValue = JBool.False
-
     override def ytrue: JValue = JBool.True
-
     override def ybool(b: Boolean): JValue = JBool(b)
-
     override def ydouble(d: Double): JValue = JDouble(d)
-
     override def ylong(l: Long): JValue = JLong(l)
-
     override def yint(i: Int): JValue = JInt(i)
-
     override def ybigdecimal(i: BigDecimal): JValue = JDecimal(i)
-
     override def ystring(s: String): JValue = JString(s)
-
     override def yarray(vs: Iterable[JValue]): JValue = JArray(vs.toList)
-
     override def yobject(vs: Iterable[(String, JValue)]): JValue = JObject(
       vs.map { case (k, v) => JField(k, v) }.toList
     )
+  }
 
+  implicit object Json4sVisitable extends Visitable[JValue] {
+    override def visit[O](t: JValue, visitor: Visitor[JValue, O]): O = t match {
+      case JObject(obj)  => visitor.onObject(obj.toMap)
+      case JSet(set)     => visitor.onArray(set.toVector)
+      case JString(s)    => visitor.onString(s)
+      case JBool(value)  => visitor.onBoolean(value)
+      case JDecimal(num) => visitor.onNumber(YamlNumber(num))
+      case JArray(arr)   => visitor.onArray(arr.toVector)
+      case JInt(num)     => visitor.onNumber(YamlNumber(num.toLong))
+      case JNothing      => visitor.onNull
+      case JNull         => visitor.onNull
+      case JDouble(num)  => visitor.onNumber(YamlNumber(num))
+      case JLong(num)    => visitor.onNumber(YamlNumber(num))
+    }
   }
 }
