d83dda057d780fe240d574d386e73a655e135eb2
Hossein Naderi <hossein-naderi@hotmail.com>
2023-09-22 12:30:40 +0330

    Fixed website

--- project/Yaml4sSitePlugin.scala
+++ project/Yaml4sSitePlugin.scala
@@ -14,20 +14,24 @@ import org.typelevel.sbt.TypelevelVersioningPlugin.autoImport.*
 import laika.sbt.LaikaPlugin.autoImport.*
 import sbt._
 import sbt.Keys._
+import cats.data.NonEmptyList
 
 object Yaml4sSitePlugin extends AutoPlugin {
   override def requires: Plugins = TypelevelSitePlugin
 
-  override def projectSettings: Seq[Def.Setting[_]] = Seq(
-    tlSiteRelatedProjects := Seq(
+  private val relatedProjects = NonEmptyList
+    .of(
       "Scala K8S" -> url("https://github.com/hnaderi/scala-k8s"),
       "Circe" -> url("https://github.com/circe/circe"),
       "ZIO-json" -> url("https://github.com/zio/zio-json"),
       "Spray json" -> url("https://github.com/spray/spray-json"),
       "Play json" -> url("https://github.com/playframework/play-json"),
       "Json4s" -> url("https://github.com/json4s/json4s")
-    ),
-    tlSiteHeliumConfig ~= {
+    )
+    .map { case (title, url) => TextLink.external(url.toString, title) }
+
+  override def projectSettings: Seq[Def.Setting[_]] = Seq(
+    tlSiteHelium ~= {
       _.site
         .favIcons(Favicon.internal(Root / "logo.svg", "32x32"))
         .site
@@ -38,6 +42,10 @@ object Yaml4sSitePlugin extends AutoPlugin {
             IconLink
               .external("https://github.com/hnaderi/yaml4s/", HeliumIcon.github)
           )
+        )
+        .site
+        .mainNavigation(appendLinks =
+          Seq(ThemeNavigationSection("Related projects", relatedProjects))
         )
 
     }
